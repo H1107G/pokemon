800652a15dea46a7e160a204272f85ca3ea8ff40
H1107G <accsignin9@gmail.com>
2025-07-29 15:44:14 +0800

    updatePokemonQuantity

--- pokemon.php
+++ pokemon.php
@@ -92,7 +92,7 @@
 
     <script>
         function loadapi(){
-            fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0')
+            fetch('https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0')
             .then(response => { 
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
